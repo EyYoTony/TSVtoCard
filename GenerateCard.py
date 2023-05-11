1172760370071ec3d9c576678e8bca219d05f2f3
alex swanson <aswanson076@gmail.com>
2023-05-11 13:22:13 -0400

    made cardGenerate a function

--- GenerateCard.py
+++ GenerateCard.py
@@ -30,76 +30,79 @@ img_url = response['data'][0]['url']
 # TODO
 # Border Based on Elemental Type ? (split for 2 color?)
 # Add Elemental Symbol?
-
-# CARD ATTRIBUTES
-# will probably double card size to accomodate 512x512 images
-width = 250
-height = 350
-border = 12
-title_height = 30
-desc_height = 120
-border_color = (124, 124, 124)
-desc_color = (73, 121, 173)
-title_color = (108, 175, 211)
-text_color = (0,0,0)
-font = cv2.FONT_HERSHEY_SIMPLEX
-title_text = df.iloc[0].Title
-desc_text = df.iloc[0].Desc
-
-
-# generate card
-# (B, G, R) colors
-img = np.zeros((height, width, 3), dtype=np.uint8)
-
-# -- add image to card --
-# TODO use stable diffusion generated img
-card_img = cv2.imread('C:\\Users\\aswan\PycharmProjects\CardGenerator\ActionCardArt\\test_dk.png')
-img[0:width, 0:width] = card_img[0:width, 0:width]
-
-# -- add border to card --
-# border left -> right -> top -> bottom
-img[:, 0:border] = border_color
-img[:, width-border:width] = border_color
-img[0:border, :] = border_color
-img[height-border:height, :] = border_color
-
-# -- add text desc --
-# color in desc space
-img[height-(border+desc_height):height-border, border:width-border] = desc_color
-# add text
-textsize = cv2.getTextSize(desc_text, font, 0.5, 1)[0]
-txt_y = height-(border+desc_height) + 40
-txt_x = (width - textsize[0]) / 2
-## text wrap to stop desc from overflowing off the side
-# used this as reference https://stackoverflow.com/questions/59390298/text-within-image-in-opencv
-wrapped_text = textwrap.wrap(desc_text, width=20, break_long_words=False)
-i = 0
-for line in wrapped_text:
-    textsize = cv2.getTextSize(line, font, 0.5, 1)[0]
-
-    gap = textsize[1] + 5
-    y = txt_y + i * gap
-    x = int((img.shape[1] - textsize[0]) / 2)
-    cv2.putText(img, line, (x, y), font, 0.5, text_color, 1)
-    i = i+1
+# title_text = df.iloc[0].Title
+def generateCard(df_row_in):
+    # CARD ATTRIBUTES
+    # will probably double card size to accomodate 512x512 images
+    width = 250
+    height = 350
+    border = 12
+    title_height = 40
+    desc_height = 120
+    border_color = (124, 124, 124)
+    desc_color = (73, 121, 173)
+    title_color = (108, 175, 211)
+    text_color = (0,0,0)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    title_text = df_row_in.Title
+    desc_text = df_row_in.Desc
 
 
-# -- add text title --
-# get values which will be used throughout the process (starting and end point of title)
-h1 = height-(border+(desc_height+title_height))
-h2 = height-(border+desc_height)
-# color in title space
-img[h1:h2, border:width-border] = title_color
-# add text
-# might need a conditional to reduce fontsize if text is too long (if textsize[0] > width-(border*2) then reduce fontsize double)
-textsize = cv2.getTextSize(title_text, font, 1, 2)[0]
-title_offset = (title_height - textsize[1])/2
-txt_y = (h2) - title_offset
-txt_x = (width - textsize[0]) / 2
-cv2.putText(img, title_text, (int(txt_x), int(txt_y)), font, 1, text_color, 2)
+    # generate card
+    # (B, G, R) colors
+    img = np.zeros((height, width, 3), dtype=np.uint8)
 
-cv2.imshow('a', img)
-#TODO change direct path to relative project path
-cv2.imwrite('C:\\Users\\aswan\PycharmProjects\CardGenerator\ActionCardImgs\Test3.png', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # -- add image to card --
+    # TODO use stable diffusion generated img
+    card_img = cv2.imread('C:\\Users\\aswan\PycharmProjects\TSVtoCard\ActionCardArt\\test_dk.png')
+    img[0:width, 0:width] = card_img[0:width, 0:width]
+
+    # -- add border to card --
+    # border left -> right -> top -> bottom
+    img[:, 0:border] = border_color
+    img[:, width-border:width] = border_color
+    img[0:border, :] = border_color
+    img[height-border:height, :] = border_color
+
+    # -- add text desc --
+    # color in desc space
+    img[height-(border+desc_height):height-border, border:width-border] = desc_color
+    # add text
+    textsize = cv2.getTextSize(desc_text, font, 0.5, 1)[0]
+    txt_y = height-(border+desc_height) + 25
+    txt_x = (width - textsize[0]) / 2
+    ## text wrap to stop desc from overflowing off the side
+    # used this as reference https://stackoverflow.com/questions/59390298/text-within-image-in-opencv
+    wrapped_text = textwrap.wrap(desc_text, width=25, break_long_words=False)
+    i = 0
+    for line in wrapped_text:
+        textsize = cv2.getTextSize(line, font, 0.5, 1)[0]
+
+        gap = textsize[1] + 5
+        y = txt_y + i * gap
+        x = int((img.shape[1] - textsize[0]) / 2)
+        cv2.putText(img, line, (x, y), font, 0.5, text_color, 1)
+        i = i+1
+
+
+    # -- add text title --
+    # get values which will be used throughout the process (starting and end point of title)
+    h1 = height-(border+(desc_height+title_height))
+    h2 = height-(border+desc_height)
+    # color in title space
+    img[h1:h2, border:width-border] = title_color
+    # add text
+    # might need a conditional to reduce fontsize if text is too long (if textsize[0] > width-(border*2) then reduce fontsize double)
+    textsize = cv2.getTextSize(title_text, font, 1, 2)[0]
+    title_offset = (title_height - textsize[1])/2
+    txt_y = (h2) - title_offset
+    txt_x = (width - textsize[0]) / 2
+    cv2.putText(img, title_text, (int(txt_x), int(txt_y)), font, 1, text_color, 2)
+
+    cv2.imshow('a', img)
+    #TODO change direct path to relative project path
+    cv2.imwrite(f'C:\\Users\\aswan\PycharmProjects\TSVtoCard\ActionCardImgs\\{title_text}.png', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+generateCard(df.iloc[3])
